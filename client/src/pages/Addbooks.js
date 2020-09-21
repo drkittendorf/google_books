4d2b87f8d72f8e325fe9cef2821bd7eee07ee8c6
drkittendorf <60675599+drkittendorf@users.noreply.github.com>
2020-09-20 21:06:10 -0500

    up

--- client/src/pages/Addbooks.js
+++ client/src/pages/Addbooks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 // import DeleteBtn from "../components/DeleteBtn";
 import API from "../utils/API";
 // import { Link } from "react-router-dom";
@@ -8,43 +8,56 @@ import { Input, TextArea, FormBtn } from "../components/Form";
 
 function Books() {
   // Setting our component's initial state
-  const [books, setBooks] = useState([])
-  const [formObject, setFormObject] = useState({})
+//   const [books, setBooks] = useState([])
+const initialFormObjectState = {
+    title: "Title (required)",
+    author: "Author (required",
+    synopsis: ""
+  };
+
+const [formObject, setFormObject] = useState({...initialFormObjectState})
   
   // Load all books and store them with setBooks
-  useEffect(() => {
-    loadBooks()
-  }, [])
+//   useEffect(() => {
+//     loadBooks()
+//   }, [])
+ 
+ 
+  const clearState = () => {
+    setFormObject({...initialFormObjectState });
+  };
 
   // Loads all books and sets them to books
   function loadBooks() {
     API.getBooks()
-      .then(res => setBooks(res.data))
+    //   .then(res => setBooks(res.data))
+      .then(clearState)
       .catch(err => console.log(err));
-  };
-
-  // Handles updating component state when the user types into the input field
-  function handleInputChange(event) {
-    const { name, value } = event.target;
-    setFormObject({...formObject, [name]: value})
-    // console.log(formObject)
   };
 
   // When the form is submitted, use the API.saveBook method to save the book data
   // Then reload books from the database
-  function handleFormSubmit(event) {
-    event.preventDefault();
+  function handleFormSubmit(e) {
+    e.preventDefault();
     if (formObject.title && formObject.author) {
       API.saveBook({
         title: formObject.title,
         author: formObject.author,
         synopsis: formObject.synopsis
       })
-        .then(console.log("book has been submitted to list") )  
+        .then(console.log("book has been submitted to list") ) 
         .then(res => loadBooks())
         .catch(err => console.log(err));
     }
   };
+   
+   // Handles updating component state when the user types into the input field
+   function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormObject({...formObject, [name]: value})
+    // console.log(formObject)
+  };
+
 
     return (
       <Container fluid>
@@ -57,17 +70,17 @@ function Books() {
               <Input
                 onChange={handleInputChange}
                 name="title"
-                placeholder="Title (required)"
+                placeholder={formObject.title}
               />
               <Input
                 onChange={handleInputChange}
                 name="author"
-                placeholder="Author (required)"
+                placeholder={formObject.author}
               />
               <TextArea
                 onChange={handleInputChange}
                 name="synopsis"
-                placeholder="Synopsis (Optional)"
+                placeholder={formObject.synopsis}
               />
               <FormBtn
                 disabled={!(formObject.author && formObject.title)}
